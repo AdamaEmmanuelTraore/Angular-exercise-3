01835f0f3fc85c7dfd3d20249ada6dae9f458e61
Traore Adama Emmanuel <adama.emmanuel.traore.11@gmail.com>
2022-04-10 02:30:05 +0200

    Angular Ac Milan

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<parent-component></parent-component>'
+  template: `
+    <parent-component></parent-component>
+  `
 })
 export class AppComponent {
   title = 'Project_3';
